810232b29449772365d80d517b5bb38c326c4949
Simon Kirk <124763876+simonkirk007@users.noreply.github.com>
2023-04-10 07:23:17 +0000

    1

--- assets/custom.js
+++ assets/custom.js
@@ -64,3 +64,46 @@ $(".arrival-slider").slick({
       // instead of a settings object
     ],
   });
+
+
+  $(".best-seller-slider").slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    arrows: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow:
+    '<div class="slick-prev"><i class="fal fa-arrow-circle-left"></i></div>',
+  nextArrow:
+    '<div class="slick-next"><i class="fal fa-arrow-circle-right"></i></div>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
